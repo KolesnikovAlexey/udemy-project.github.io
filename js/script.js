d2846249debed9442a213d9ec077a71ec1d94ab1
Alex <kolesnikov.alex81@gmail.com>
2023-02-15 19:40:09 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// alert('Hello');
+//
+// const result = confirm("Are you here?");
+//
+// console.log(result);
+
+// const answer = prompt("How old are you?", "18 is old");
+// console.log(typeof (answer));
+
+// const answer = [];
+//
+// answer[0] = prompt("Как тебя завут?", "Ivan");
+// answer[1] = prompt("Как твоя фамилия?", " Ivanov");
+// answer[2] = prompt("Сколько тебе лет?", " 00");
+// document.write(answer);
+// console.log(answer);
+
+// const category = 'toys';
+// console.log(`https://test.com/${category}/5`)
+
+let user = "Alex";
+alert(`Hello my friend ${user}`);
